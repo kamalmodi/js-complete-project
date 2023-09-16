e82bb7592735e8a6654ff05048801757d9c8e02b
kamalmodi <140087620+kamalmodi@users.noreply.github.com>
2023-09-16 08:06:15 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "kamal.modi@gmail.com"
+var password = "12345"
+accountCity = "Delhi"
+
+// accountId=2 
+console.log(accountId);
+
+accountEmail= "hdfc@123"
+password = "23455"
+accountCity = "jaipur"
+console.table ([accountId,accountEmail,password,accountCity])
+/*  prefer var is not to use 
+*/ 
