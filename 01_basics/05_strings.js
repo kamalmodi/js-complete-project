0a9962ea88a3d7b863e8bd193c7482ce793831f3
kamalmodi <140087620+kamalmodi@users.noreply.github.com>
2023-09-18 15:27:42 +0000

    basic types of string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+const name = " kamal"
+ const repoCount= 50
+ // console.log (name +repoCount + "value")
+
+  //console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);
+
+  const gameName= new String('kamal');
+   console.log(gameName[0]);
+   console.log(gameName.length);
+   console.log(gameName.charAt(2));
+   console.log(gameName.indexOf('m'));
+   
+
