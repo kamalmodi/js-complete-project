8336c2163e2593dafd0200287041b1972abf915d
kamalmodi <140087620+kamalmodi@users.noreply.github.com>
2023-09-19 13:04:40 +0000

    basic dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,14 @@
+let myDate= new Date();
+//console.log(myDate);
+/*
+console.log(myDate.toString());          
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+*/
+
+
+
